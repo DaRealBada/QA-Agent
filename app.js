98dcc694f87731b31d1d89aac0b0112d87ae8bbc
Tolu Bada <142679079+DaRealBada@users.noreply.github.com>
2025-08-01 10:47:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 8080;
 
 app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.post('/create-task', (req, res) => {
   const prTitle = req.body.pr_title;
